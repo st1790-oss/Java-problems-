660aaf0a3cb5552d90658fa9892cef3ad4c19729
Shulpani Tripathi <st1790@srmist.edu.in>
2026-03-09 21:46:25 +0530

    Clean up PoundsToKilograms.java
    Removed commented lines and fixed output message.

--- PoundsToKilograms.java
+++ PoundsToKilograms.java
@@ -3,14 +3,9 @@ import java.util.Scanner;
 public class PoundsToKilograms {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            // 
             System.out.print("Enter weight in pounds: ");
             double pounds = sc.nextDouble();
-
-            // Conversion
             double kilograms = pounds / 2.2;
-
-            // Outpu
             System.out.println("The weight of the person in pound is " + pounds + " and in kg is " + kilograms);
         }
     }

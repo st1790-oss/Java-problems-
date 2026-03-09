c70f4bca825f601d9d10078df554d3dc9c470ff4
Shulpani Tripathi <st1790@srmist.edu.in>
2026-03-09 21:27:00 +0530

    Add Kmtomiles.java program

---
+++ Kmtomiles.java
@@ -0,0 +1,10 @@
+public class Kmtomiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double milePerKm = 1.6;
+
+        double miles = km * milePerKm;
+
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}

2dec6e2887c465261ad771ce6508cc8f02fc97a2
Shulpani Tripathi <st1790@srmist.edu.in>
2026-03-09 21:44:58 +0530

    Clean up comments in BasicCalculator
    Removed comments for user input and results printing.

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -4,20 +4,16 @@ public class BasicCalculator {
     public static void main(String[] args) {
         
         try (Scanner sc = new Scanner(System.in)) {
-            // Taking user inputs
             System.out.print("Enter first number: ");
             float number1 = sc.nextFloat();
 
             System.out.print("Enter second number: ");
             float number2 = sc.nextFloat();
-
-            // Performing arithmetic operations
             float addition = number1 + number2;
             float subtraction = number1 - number2;
             float multiplication = number1 * number2;
             float division = number1 / number2;
 
-            // Printing results
             System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
                     + number1 + " and " + number2 + " is "
                     + addition + ", " + subtraction + ", " + multiplication + ", and " + division);

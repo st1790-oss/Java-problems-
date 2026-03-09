fd2eac6a95b2bfe765d19be9cec30887889ed740
Shulpani Tripathi <st1790@srmist.edu.in>
2026-03-09 21:45:58 +0530

    delete all comments
    Corrected a comment typo and adjusted output formatting.

--- PoundsToKilograms.java
+++ PoundsToKilograms.java
@@ -10,7 +10,7 @@ public class PoundsToKilograms {
             // Conversion
             double kilograms = pounds / 2.2;
 
-            // Output
+            // Outpu
             System.out.println("The weight of the person in pound is " + pounds + " and in kg is " + kilograms);
         }
     }
